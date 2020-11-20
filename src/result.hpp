3a377bd32f4ce992d7dfa132584f74cd4ac1f845
Henrique Andrade <henrique.andrade22@hotmail.com>
2020-11-19 22:24:27 -0300

    Create result struct

---
+++ src/result.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+struct Result
+{
+    string algorithm;
+    string evolution;
+    int hits;
+    int faults;
+    int requests;
+    float error_rate;
+};
